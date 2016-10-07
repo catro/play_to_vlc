d9e8cc3bbe2f09ac621cde351225b11811e57303
catro <catro@qq.com>
2016-10-07 21:03:37 +0800

    Add js for web browser. Run the you-get in thread instead of cmd window.

---
+++ PlayToVLC.js
@@ -0,0 +1,1 @@
+javascript:void((function(){var win=window.open('http://localhost:8000/'+window.location.href);})())
--- http_server.py
+++ http_server.py
@@ -3,23 +3,31 @@ from SimpleHTTPServer import SimpleHTTPRequestHandler
 import SocketServer
 import os
 import mutex
-import threading
+import thread
 from datetime import timedelta
 from datetime import datetime
 
 PORT = 8000
 
+def process_url(url):
+    os.system('you-get -p "vlc -f --http-port=8081" "' + url + '"')
+    #os.system('you-get -p "python play_to_kodi.py" "' + url + '"')
+
 class http_server(SimpleHTTPRequestHandler):
     def do_GET(self):
-        print "Got GET request %s" % (self.path[1:])
         if (self.path[1:5] == 'http'):
             url = self.path[1:].replace('/m.', '/www.')
-            os.system('start you-get -p vlc "' + url + '"')
+            print "Got GET request %s" % (url)
+            thread.start_new_thread(process_url, (url,))
+            #os.system('start you-get -p vlc "' + url + '"')
             #os.system('you-get -p "python play_to_kodi.py" "' + url + '"')
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write("<html><head><script>void(function back(){window.history.back()})()</script></head><body /></html>")
+        try:
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write("<html><head><script>void(function back(){window.close()})()</script></head><body /></html>")
+        except:
+            pass
 
 if __name__ == "__main__":
     httpd = SocketServer.TCPServer(("", PORT), http_server)
